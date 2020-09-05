ba45eb743063f37ef530e68ec280a462152f5e8d
Cameron Campos <cameron.campos.0327@gmail.com>
2020-09-05 06:49:35 -0700

    Edit to SECTION007

---
+++ SECTION007 - Copy/backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParsesr = require('body-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const connection = mongoose.connection;
+mongoose.connect("mongodb+srv://CCampos:Potato@42!@frameworktv.2xbn4.gcp.mongodb.net/FrameworkTV?r" +
+        "etryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+connection.once('open', () => {
+    console.log("MongoDB connection succsessful.")
+})
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
